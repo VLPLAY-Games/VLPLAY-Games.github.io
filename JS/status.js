dd5df878e1704acf072cef2aa781bc9a632fd3ac
VL_PLAY <85053463+VLPLAY-Games@users.noreply.github.com>
2023-12-19 08:46:41 +0300

    Update status.js

--- JS/status.js
+++ JS/status.js
@@ -1,13 +1,13 @@
 const status = {
     'botVersion': "1.4.2",
     'qlakeVersion': "2.0.1",
-    'xcVersion': "0.5 Beta",
+    'xcVersion': "0.5.0",
     'sdVersion': "0.1 Demo",
     'siteVersion': "1.0.1",
 
     'botDate': "13.10.2023",
     'qlakeDate': "03.11.2023",
-    'xcDate': "15.12.2023",
+    'xcDate': "18.12.2023",
     'sdDate': "30.11.2023",
     'siteDate': "08.12.2023",
 };
