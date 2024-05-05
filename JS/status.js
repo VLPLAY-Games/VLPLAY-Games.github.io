0dd0bc619124bbd3956b19d784385df9d4b565f0
Vlad Play <mr.bonie39@gmail.com>
2024-05-05 16:18:01 +0300

    Update

--- JS/status.js
+++ JS/status.js
@@ -1,13 +1,13 @@
 const status = {
     'botVersion': "1.5.0",
     'qlakeVersion': "2.0.1",
-    'xcVersion': "0.8.0",
+    'xcVersion': "0.9.0",
     'sdVersion': "Demo 2",
     'siteVersion': "1.0.3",
 
     'botDate': "17.01.2024",
     'qlakeDate': "03.11.2023",
-    'xcDate': "17.03.2024",
+    'xcDate': "05.05.2024",
     'sdDate': "22.04.2024",
     'siteDate': "17.01.2024",
 };
