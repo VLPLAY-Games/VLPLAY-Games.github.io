4ac8dbb6b790708017dc31677c8b417ec10f8adf
VL_PLAY <mr.bonie39@gmail.com>
2025-10-07 12:02:32 +0300

    styles update ready 75%

--- JS/status.js
+++ JS/status.js
@@ -33,7 +33,7 @@ const status = {
     'sdAndVersion': "1.1.0.120 (Test)",
     'sdAndDate': "30.03.2025",
 
-    'siteVersion': "2.0.0 preview 3",
+    'siteVersion': "2.0.0 preview 4",
     'siteDate': "07.10.2025",
 };
 
